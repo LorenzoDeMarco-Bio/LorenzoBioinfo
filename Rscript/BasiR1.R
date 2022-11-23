eef93f39df963c558e1f65252bfd7c225cf92586
LorenzoDeMarco-Bio <80900952+LorenzoDeMarco-Bio@users.noreply.github.com>
2022-11-23 20:04:03 +0100

    Add files via upload

---
+++ Rscript/BasiR1.R
@@ -0,0 +1,41 @@
+
+##Operazioni matematiche
+#Addizione
+3+5   
+#Sottrazione
+10-4  
+#Moltiplicazione
+2*5
+#Divisione
+9/3
+
+#Quoziente
+10%%3
+#Esponenziale
+2**3
+2^3
+
+## Disuguaglianze 
+#Uguale
+3==3
+
+5-2==2+1
+
+#Maggiore e maggiore uguale
+5>3
+6>=4
+
+#Minore e minore uguale
+10<100
+
+#Operazioni logiche
+# AND  &
+TRUE & FALSE 
+
+# OR |
+TRUE | FALSE
+
+# NOT !=
+TRUE != TRUE
+
+
---
+++ Rscript/BasiR2.R
@@ -0,0 +1,24 @@
+##Funzioni pre esistenti
+
+# Absolute value
+abs()
+
+# Radice quadrata
+sqrt()
+
+# Ceiling and floor
+ceiling()   # Nearest integer >= x
+floor()     # Nearest integer <= x
+
+# Trunc 
+trunc()
+
+# Natural Logarithm
+log()
+
+# Base 10 Logarithm
+log10()
+
+# Exp
+exp()
+
