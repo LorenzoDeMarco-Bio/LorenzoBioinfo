04fdc4ea91bd844e022ab2078546d89a81c8688b
LorenzoDeMarco-Bio <80900952+LorenzoDeMarco-Bio@users.noreply.github.com>
2022-12-02 08:43:17 +0100

    Add files via upload

---
+++ Rscript/BasiR3.R
@@ -0,0 +1,49 @@
+
+# Assegnazione di una variabile
+
+x<-3
+x<-5
+x
+# Tipi di dati
+
+# class 
+class(x)
+
+x<-as.integer(5)
+class(x)
+# complessi
+y<-3+1i
+class(y)
+
+# logical
+x<-T
+z<-F
+class(x)
+
+# character (strings)
+x<-"hey"
+class(x)
+
+# Domande
+x<-"Ciao, sono Lorenzo"
+is.numeric(x)
+
+is.character(x)
+is.logical(x)
+is.complex(x)
+
+
+
+# CASTING (cambiare il tipo di dato)
+
+y<-"0.50"
+
+as.numeric(y)
+
+
+
+
+
+
+
+
